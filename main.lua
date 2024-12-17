2073475b1753b66b51e1769c4c2f06d584f272d3
The Birthday Coyote :] <constachugga@gmail.com>
2024-12-17 09:12:07 -0800

    d

--- main.lua
+++ main.lua
@@ -1,4 +1,3 @@
-
 local textures = {}
 
 local function tex (name)
@@ -14,38 +13,67 @@ end
 local wide = -1
 local tall = -1
 
-local appsurface
+
+local appsurface = nil
+local pevsurface = nil
+
+local function createScreenBuffer ()
+	return love.graphics.newCanvas(wide, tall)
+end
 
 local function draw ()
-	local text = "WOW!!! %i %i"
+	local mx, my = love.mouse.getPosition()
+	local text = "WOW!!! %i %i\nmouse (%i %i)"
+	local room = tex 'def'
 	
+
+	local picWide = room:getWidth()
+
+	local ofs = (mx / wide) * (picWide - wide)
+
+	love.graphics.push()
+	love.graphics.translate(-ofs, 0)
 	love.graphics.draw(room, 0, 0)
-	love.graphics.print(text:format(wide, tall), 320, 240)
+	love.graphics.pop()
+	
+	
+	love.graphics.print(text:format(wide, tall, mx, my), 320, 240)
+end
+
+local function tryRenderAppsurface ()
+	if not appsurface then
+		appsurface = createScreenBuffer()
+	end
+	appsurface:renderTo(draw)
 end
 
 function love.draw ()
-	local room = tex 'def'
-
-	do
-		local wwide, wtall = love.window.getMode()
-	
-		if wwide ~= wide or wtall ~= tall then
-			if appsurface then
-				appsurface:release()
-				appsurface = nil
-			end
-			wide = wwide
-			tall = wtall
+	local wwide, wtall = love.window.getMode()
+	if wwide ~= wide or wtall ~= tall then
+		if appsurface then
+			appsurface:release()
+			appsurface = nil
 		end
+		wide = wwide
+		tall = wtall
 	end
+	
+	tryRenderAppsurface()
 
-	if not appsurface then
-		appsurface = love.graphics.newCanvas(wide, tall)
+	if not pevsurface then
+		pevsurface = createScreenBuffer()
+		pevsurface:renderTo(function ()
+			love.graphics.setBlendMode 'replace'
+			love.graphics.draw(appsurface, 0, 0)
+			love.graphics.setBlendMode 'alpha'
+		end)
+	else
+		pevsurface:renderTo(function ()
+			
+		end)
 	end
+	
+	love.graphics.draw(appsurface, 0, 0)
 
-	if appsurface then
-		appsurface:renderTo(draw)
-		love.graphics.draw(appsurface, 0, 0)
-	end
 
 end
