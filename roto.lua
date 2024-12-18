60f3e28e958056f9f7c221f164c0e8dfc5e883dc
The Birthday Coyote :] <constachugga@gmail.com>
2024-12-17 16:18:29 -0800

    a

--- roto.lua
+++ roto.lua
@@ -14,8 +14,22 @@ local viewYaw = 0
 local viewRetard = 1/6
 
 function love.mousemoved (x, y, dx, dy)
-	viewPitch = clampSym(viewPitch + dy * viewRetard, 90)
-	
+	if not love.mouse.getRelativeMode() then
+		return
+	end
+
+	viewPitch = clampSym(viewPitch - dy * viewRetard, 90)
+	viewYaw = viewYaw + dx * viewRetard + 180
+	-- yaw(n)
+	while viewYaw < 0 do viewYaw = viewYaw + 360 end
+	while viewYaw >= 360 do viewYaw = viewYaw - 360 end
+	viewYaw = viewYaw - 180
+end
+
+function love.keypressed (k)
+	if k == 'escape' then
+		love.mouse.setRelativeMode(not love.mouse.getRelativeMode())
+	end
 end
 
 function love.load ()
@@ -29,5 +43,6 @@ function love.update (dt)
 end
 
 function love.draw ()
-
+	local txt = ('%.8f %.8f'):format(viewPitch, viewYaw)
+	love.graphics.print(txt, 320, 240)
 end
